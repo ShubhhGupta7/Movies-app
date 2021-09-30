7e5d94b0008f3578c284985bfcff290bec92c4e6
ShubhhGupta7 <shubhgupta036@gmail.com>
2021-09-30 10:10:20 +0530

    add subscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,57 @@ import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-	  <div className = "main">
+class App extends React.Component {  
+	
+	componentDidMount() {
+		// In real world senerio
+		// make an api call
+		// and dispatch action in store
 
-		  <div className ="tabs">
-			  <div className ="tab">Movies</div>
-			  <div className ="tab">Favourites</div>
-		  </div>
+		const {store} = this.props;
 
-		  <div className ="list">	
-			{data.map((movie, index) => (
-				<MovieCard movie = {movie} key = {`movies-${index}`} />
-			))}	  		
-		  </div>
-	  </div>
-    </div>
-  );
+		// Subscribe method is basically a method which is called when ever any dispatch action changes our state and it takes a function as its argument,
+		store.subscribe(() => {
+			console.log('state updated!');
+			this.forceUpdate();
+
+			// Force update must be avoided for our easiness we have only used here.
+		});
+
+		store.dispatch({
+			type: 'ADD_MOVIES',
+			movies: data
+		});
+		// Here we are getting data from file or api call and we will be sending an action for an indent to change the state using dispatch.
+
+		// Now as our state is changed but we are doing nothing to rerender our component. 
+
+	}
+
+	render() {
+		const {store} = this.props;
+		const movies = store.getState();
+
+		console.log(movies, "movies array");
+		return (
+			<div className="App">
+			  <Navbar />
+			  <div className = "main">
+		
+				  <div className ="tabs">
+					  <div className ="tab">Movies</div>
+					  <div className ="tab">Favourites</div>
+				  </div>
+		
+				  <div className ="list">	
+					{movies.map((movie, index) => (
+						<MovieCard movie = {movie} key = {`movies-${index}`} />
+					))}	  		
+				  </div>
+			  </div>
+			</div>
+		  );
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,21 +7,21 @@ import App from './components/App';
 import movies from './Reducers';
 
 const store = createStore(movies);
-console.log('before state', store.getState());
-store.dispatch({
-  type: 'ADD_MOVIES',
-  movies: [
-    {
-      name: 'Superman'
-    }
-  ]
-})
-console.log('after state', store.getState());
+// console.log('before state', store.getState());
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [
+//     {
+//       name: 'Superman'
+//     }
+//   ]
+// })
+// console.log('after state', store.getState());
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store = {store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
